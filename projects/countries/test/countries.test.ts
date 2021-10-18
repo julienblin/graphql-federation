a3f21e504885062ed9ceb4be86db3c27f39859bd
Julien Blin <julien.blin@gmail.com>
2021-10-18 16:38:24 +0000

    WIP - jest

--- projects/countries/test/countries.test.ts
+++ projects/countries/test/countries.test.ts
@@ -1,8 +1,21 @@
 import { gql } from "apollo-server-core";
+import * as nock from "nock";
+import { basename, dirname, join } from "path";
 import { server } from "../src/server";
 
 describe("Fetch countries", () => {
   it("should be ok", async () => {
+    nock.back.fixtures = join(dirname(expect.getState().testPath), "__nock__");
+    nock.back.setMode("record");
+    const { nockDone, context } = await nock.back(
+      basename(expect.getState().testPath, ".ts") +
+        "." +
+        expect
+          .getState()
+          .currentTestName.replace(/[^a-z0-9]/gi, "_")
+          .toLowerCase()
+    );
+
     const result = await server.executeOperation({
       query: gql`
         query GetCountries {
@@ -13,7 +26,9 @@ describe("Fetch countries", () => {
         }
       `,
     });
+    nockDone();
 
+    expect(result.errors).toBeFalsy();
     expect(result.data.countries.length).toBeTruthy();
   });
 });
