e4a1807db6e241d4b4ac1eec3b1ecee2439256ee
Julien Blin <julien.blin@gmail.com>
2021-10-21 12:50:03 +0000

    Minor housekeeping.

--- projects/gateway/src/server.ts
+++ projects/gateway/src/server.ts
@@ -3,8 +3,8 @@ import { ApolloServer } from "apollo-server";
 
 const gateway = new ApolloGateway({
   serviceList: [
-    { name: "Countries", url: "http://localhost:4002/graphql" },
-    { name: "Covid19", url: "http://localhost:4001/graphql" },
+    { name: "Countries", url: "http://localhost:4001/graphql" },
+    { name: "Covid19", url: "http://localhost:4002/graphql" },
     { name: "Population", url: "http://localhost:4003/graphql" },
   ],
 });
