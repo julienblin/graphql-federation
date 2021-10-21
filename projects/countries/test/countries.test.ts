43a62028e5a1665fa07b92aac9624906fbeca0e1
Julien Blin <julien.blin@gmail.com>
2021-10-21 12:12:49 +0000

    Testing countries entity resolver

--- projects/countries/test/countries.test.ts
+++ projects/countries/test/countries.test.ts
@@ -8,7 +8,7 @@ describe("countries", () => {
     const result = await captureHttp(async () => {
       return await server.executeOperation({
         query: gql`
-          query GetCountries {
+          query {
             countries {
               countryCode
               name
@@ -26,7 +26,7 @@ describe("countries", () => {
     const result = await captureHttp(async () => {
       return await server.executeOperation({
         query: gql`
-          query GetCountries {
+          query {
             country(countryCode: "CA") {
               countryCode
               name
@@ -38,6 +38,35 @@ describe("countries", () => {
 
     expect(result.errors).toBeFalsy();
     expect(result.data.country).toMatchObject(<Country>{
+      countryCode: "CA",
+      name: "Canada",
+    });
+  });
+
+  it("should fetch country entity", async () => {
+    const result = await captureHttp(async () => {
+      return await server.executeOperation({
+        query: gql`
+          query ($representations: [_Any!]!) {
+            _entities(representations: $representations) {
+              ... on Country {
+                countryCode
+                name
+              }
+            }
+          }
+        `,
+        variables: {
+          representations: {
+            __typename: "Country",
+            countryCode: "CA",
+          },
+        },
+      });
+    });
+
+    expect(result.errors).toBeFalsy();
+    expect(result.data._entities[0]).toMatchObject(<Country>{
       countryCode: "CA",
       name: "Canada",
     });
